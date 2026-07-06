8d9cc8716e0289b68d4c2c2fca29101fc01081ef
JadhavSrushti1544 <jadhavsrushti1544@gmail.com>
2026-07-06 16:56:53 +0530

    Add user.go

---
+++ internal/models/user.go
@@ -0,0 +1,28 @@
+package models
+
+import "time"
+
+type User struct {
+
+	ID int `json:"id`
+	Email string `json:"email`
+	PasswordHash string `json:"-"` //never expose
+	Role string `json:"role"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
+
+type RegisterRequest struct {
+	Email string `json:"email" binding:"required,email"`
+	Password string `json:"password" binding:"required,min=8"`
+}
+
+type LoginRequest struct {
+	Email string `json:"email" binding:"required"`
+	Password string `json:"password" binding:"required"`
+}
+
+type LoginResponse struct {
+	Token string `json:"token"`
+	User User `json:"user"`
+}
