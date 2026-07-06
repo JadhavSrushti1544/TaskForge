5158e9ad26428563b8682f2c005cbd393c113b8a
JadhavSrushti1544 <jadhavsrushti1544@gmail.com>
2026-07-06 18:18:08 +0530

    Add config.go

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,6 +1,42 @@
 package config
 
-type Config struct{
-	DBHost string
-	DBPort string
+import (
+	"database/sql"
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/joho/godotenv"
+)
+
+func LoadEnv() {
+	godotenv.Load()
+}
+
+func InitDB() {
+	dsn := fmt.Sprintf(
+		"host=%s port=%s password=%s dbname=%s sslmode=disable",
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_PORT"),
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_NAME"),
+	)
+
+	db, err := sql.Open("postgres", dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	if err := db.Ping(); err != nil {
+		return nil, err
+	}
+	fmt.Println("✅ Database connection successful")
+	return db, nil
+
+	db.SetMaxOpenConns(10)
+	db.SetMaxIdleConns(5)
+	db.SetConnMaxLifetime(5 * time.Minute)
+
+	return db, nil
 }
