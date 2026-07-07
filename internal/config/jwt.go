6a815012a03866dedb315f1dbe6a6f21d83f9d6c
JadhavSrushti1544 <jadhavsrushti1544@gmail.com>
2026-07-08 01:31:37 +0530

    Add jwt.go

---
+++ internal/config/jwt.go
@@ -0,0 +1,31 @@
+package config
+
+import (
+	"os"
+
+	"github.com/golang-jwt/jwt"
+)
+
+var jwtSecret = []byte(os.Getenv("JWT_SECRET"))
+
+// GenerateToken creates a JWT token valid for 24 hours
+func GenerateToken(userID int, email string) (string, error) {
+	token :=  jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"user_id": userID,
+		"email": email,
+		"exp":time.Now().Add(24*time.Hour).Unix(),
+	})
+
+	// VerifyToken validates a JWT token
+	func VerifyToken(tokenString string) (int, error){
+		token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+			return jwtSecret, nil
+		})
+		if err != nil || !token.Valid {
+			return 0, err
+		}
+		claims := token.Claims.(jwt.MapClaims)
+		userID := int(claims["user_id"].(float64))
+		return userID, nil
+	}
+}
